86d4cce92bddd15086c7af15ad6b1c0a29612c1f
ArchoMontell <lilblpy45@gmail.com>
2025-04-10 13:53:57 +0300

    Lab4 Completed

---
+++ Lab4/src/main/java/edu/prydatkin/Converter.java
@@ -0,0 +1,14 @@
+package edu.prydatkin;
+
+
+/*
+    @author lilbl
+    @project Lab4
+    @class Converter
+    @version 1.0.0
+    @since 4/10/2025 - 13.20
+*/
+    
+
+public class Converter {
+}
---
+++ Lab4/src/test/java/edu/prydatkin/ConverterTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+    @author lilbl
+    @project Lab4
+    @class ConverterTest
+    @version 1.0.0
+    @since 4/10/2025 - 13.22
+*/
+    
+
+class ConverterTest {
+  
+}
